facb5d7c7c2f2d2befa00c32e9a8787a162341a2
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-03-29 10:12:04 -0400

    js split and entry points set

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = { 
+    entry: {
+      app: './assets/js/script.js',
+      events: './assets/js/event.js',
+      schedule: './assets/js/schedule.js',
+      tickets: './assets/js/tickets.js'
+    }, 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', //the report outputs to an HTML file in the dist
+        })
+      ],
+    mode: 'development'
+};
