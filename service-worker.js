601576730b5ec5f90dc595df3b2572deea873cc8
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-03-30 09:26:08 -0400

    testing on pages

--- service-worker.js
+++ service-worker.js
@@ -23,3 +23,32 @@ self.addEventListener('install', function (e) {
       })
     )
   })
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('delete cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith (
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request);
+    })
+  )
+})
